c288fae105448d98a6e0e14211c3661e6060d22b
kapil kumar sangwan <kapilkumarsangwan@192.168.1.4>
2023-09-24 15:58:44 +0530

    added chat feature

--- rein-sticker/src/components/chat/chat/Messages.jsx
+++ rein-sticker/src/components/chat/chat/Messages.jsx
@@ -1,63 +1,72 @@
-import {useState,useContext,useEffect} from 'react'
-import {styled,Box} from '@mui/material';
+import { useState, useContext, useEffect } from 'react'
+import { styled, Box } from '@mui/material';
 import Footer from './Footer';
 import { AccountContext } from '../../../context/AccountProvider';
-import { newMessage,getMessages} from '../../../service/api';
+import { newMessage, getMessages } from '../../../service/api';
 import SingleMessage from './SingleMessage';
 
-const Wrapper=styled(Box)`
+const Wrapper = styled(Box)`
   background-image: url('https://i.pinimg.com/564x/fe/41/48/fe41486f310e4847d8b2e6b2f2ff7502.jpg');
   background-size:50%;
 `
-const Component=styled(Box)`
+const Component = styled(Box)`
   height:81.9vh;
   overflow-y:scroll;
 `
- 
-const Messages = ({person,conversation}) => {
-   
-   const {account}=useContext(AccountContext);
-   const [value,setValue]=useState('');
-   const [messages,setMessages]=useState([]);
-   
-   useEffect(()=>{
-      const getMessageDetails=async()=>{
-        const data=await getMessages(conversation._id);
-        setMessages(data);
-      }
-      conversation._id && getMessageDetails();
-   },[person.sub,conversation._id]);
+const Container = styled(Box)`
+  padding : 1px 80px;
+`
 
 
-   const sendText=async(e)=>{
-    
+
+const Messages = ({ person, conversation }) => {
+
+  const { account } = useContext(AccountContext);
+  const [value, setValue] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [new_message, setNew_Message] = useState(false);
+
+  useEffect(() => {
+    const getMessageDetails = async () => {
+      const data = await getMessages(conversation._id);
+      setMessages(data);
+    }
+    conversation._id && getMessageDetails();
+  }, [person.sub, conversation._id, new_message]);
+
+
+  const sendText = async (e) => {
+
     if (e.key === 'Enter') {
-       let message={
-          senderId:account.sub,
-          receiverId:person.sub, 
-          conversationId:conversation._id,
-          type:"text",
-          text:value,
-       }
-       
-       await newMessage(message);
-       setValue('');
+      let message = {
+        senderId: account.sub,
+        receiverId: person.sub,
+        conversationId: conversation._id,
+        type: "text",
+        text: value,
+      }
+
+      await newMessage(message);
+      setValue('');
+      setNew_Message(!new_message);
     }
 
-   }
+  }
 
   return (
     <Wrapper>
-       <Component>
-          {
-            messages.map((message)=>{
-               return (
-                 <SingleMessage message={message}></SingleMessage>
-               )
-            })
-          }
-       </Component>
-       <Footer sendText={sendText} setValue={setValue} value={value}></Footer>
+      <Component>
+        {
+          messages.map((message) => {
+            return (
+              <Container>
+                <SingleMessage message={message}></SingleMessage>
+              </Container>
+            )
+          })
+        }
+      </Component>
+      <Footer sendText={sendText} setValue={setValue} value={value}></Footer>
     </Wrapper>
   )
 }
